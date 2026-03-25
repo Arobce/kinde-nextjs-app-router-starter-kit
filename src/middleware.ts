806046032c647b80bba24695e4e44bbffb721904
Roshan Chapagain <arobce@gmail.com>
2026-03-25 17:24:36 -0500

    reset

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,19 @@
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { NextRequest, NextResponse } from "next/server";
+import {
+  withAuth,
+} from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth(
-  async function middleware(req: NextRequest) {
-    const pathname = req.nextUrl.pathname;
-
-    if (pathname === "/dashboard") {
-      const loginUrl = new URL("/api/auth/login", req.url);
-      loginUrl.searchParams.set("prompt", "none");
-      loginUrl.searchParams.set("post_login_redirect_url", "/dashboard");
-
-      return NextResponse.redirect(loginUrl);
-    }
-
-    return NextResponse.next();
+  async function middleware(req: import('next/server').NextRequest) {
   },
   {
-    publicPaths: ["/", "/api/public", "/terms", "/privacy"],
+    publicPaths: ["/", "/api/public", "/terms", "/privacy" ],
     isReturnToCurrentPage: true,
   }
 );
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Run on everything but Next internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)"
   ],
 };
